fea1f2ee521463a8815ab44e0c8cdd21c56265de
Siro <3070987+U-siro@users.noreply.github.com>
2018-11-15 18:54:53 +0900

    Much Better Protocol Handling

--- server.js
+++ server.js
@@ -3,92 +3,122 @@ const fs = require('fs')
 const tls = require('tls')
 
 Array.prototype.random = function () {
-    return this[Math.floor((Math.random()*this.length))];
-  }
+    return this[Math.floor((Math.random() * this.length))];
+}
 
 let config = JSON.parse(fs.readFileSync('config.json'))
 
 let certificateRoute = {}
 
-let defaultCertificate = {...{
-    SNICallback: function (domain, cb) {
-        let targetRoute = Object.values(config.routing).find(o => o.host.includes(domain) && o.secure)
-        if(targetRoute){
-            cb(null, tls.createSecureContext(targetRoute.secure))
-        } else {
-            cb(null, null)
+let defaultCertificate = { ...{
+        SNICallback: function (domain, cb) {
+            let targetRoute = Object.values(config.routing).find(o => o.host.includes(domain) && o.secure)
+            if (targetRoute) {
+                cb(null, tls.createSecureContext(targetRoute.secure))
+            } else {
+                cb(null, null)
+            }
         }
-    }
-}, ...config.defaultCertificate}
+    },
+    ...config.defaultCertificate
+}
 
 
 var greenlock = require('greenlock').create({
-    version: 'draft-12'
-  , server: 'https://acme-v02.api.letsencrypt.org/directory'
-  , configDir: '/tmp/acme/etc'
-  });
-  
+    version: 'draft-12',
+    server: 'https://acme-v02.api.letsencrypt.org/directory',
+    configDir: '/tmp/acme/etc'
+});
 
-function letsEncrypt(domains, callback){
+
+function letsEncrypt(domains, callback) {
     var opts = {
-        domains: domains
-      , email: 'ruby.aqour@gmail.com'
-      , agreeTos: true                  // Accept Let's Encrypt v2 Agreement
-      , communityMember: true           // Help make Greenlock better by submitting
-                                        // stats and getting updates
-      };
-      greenlock.register(opts).then(function (certs) {
+        domains: domains,
+        email: 'ruby.aqour@gmail.com',
+        agreeTos: true // Accept Let's Encrypt v2 Agreement
+            ,
+        communityMember: true // Help make Greenlock better by submitting
+        // stats and getting updates
+    };
+    greenlock.register(opts).then(function (certs) {
         callback(null, certs)
         // privkey, cert, chain, expiresAt, issuedAt, subject, altnames
-      }, function (err) {
+    }, function (err) {
         callback(err, null)
-      });
+    });
 }
 
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  var proxy = require('http-proxy-middleware');
-  var express = require('express')
-  var app = express()
-  var http = require('http')
-  var https = require('https')
-  var bodyParser = require('body-parser')
-  
+// Workers can share any TCP connection
+// In this case it is an HTTP server
+var proxy = require('http-proxy-middleware');
+var express = require('express')
+var app = express()
+var http = require('http')
+var https = require('https')
+var bodyParser = require('body-parser')
 
-  app.use('/', greenlock.middleware());
-  app.use((req, res, next) => {
-      if(!Object.values(config.routing).find(o => o.host.includes(req.headers.host))){
+
+app.use('/', greenlock.middleware());
+app.use((req, res, next) => {
+    if (!Object.values(config.routing).find(o => o.host.includes(req.headers.host))) {
         res.status(400).send('No such route')
         res.end('No such route')
         return
-      }
-      let targetRoute = JSON.parse(JSON.stringify(Object.values(config.routing).find(o => o.host.includes(req.headers.host))))
-      
-          if(req.url == '/cdn-cgi/trace'){
-              if(targetRoute.lockDetails){
-                  res.status(401).send('Tried to access hidden route')
-              } else{
-                  if(targetRoute.secure){
-                      targetRoute.secure.key = "####Censored by nProxy####";
-                  }
-                  res.json(targetRoute)
-              }
-          } else {
-              proxy({
-                  target: targetRoute.target.random(), 
-                  ws: targetRoute.websocket || true,
-                  changeOrigin: targetRoute.sendHost || true
-              })(req, res, next)
-          }
-      
-  
-  });
-  var httpServer = http.createServer(app).listen(80);
-  var httpsServer = https.createServer(defaultCertificate, app).listen(443);
-  
+    }
+    let targetRoute = JSON.parse(JSON.stringify(Object.values(config.routing).find(o => o.host.includes(req.headers.host))))
 
-  // API Server
-  // Require express and create an instance of it
+    if (req.url == '/cdn-cgi/trace') {
+        if (targetRoute.lockDetails) {
+            res.status(401).send('Tried to access hidden route')
+        } else {
+            if (targetRoute.secure) {
+                targetRoute.secure.key = "####Censored by nProxy####";
+            }
+            res.json(targetRoute)
+        }
+    } else {
+        let proxyTarget
+        let httpTargets = []
+        let httpsTargets = []
+        targetRoute.target.forEach((v) => {
+            if (v.startsWith('https://')) {
+                httpsTargets.push(v)
+            } else {
+                httpTargets.push(v)
+            }
+        })
+        if (httpsTargets.length > 0 && req.socket.server.key) {
+            // https 타겟 O, http 타겟 ?, https로 연결 -> https 타겟으로 연결
+            proxyTarget = httpsTargets.random()
+        } else if (httpTarget.length > 0 && !req.socket.server.key) {
+            // https 타겟 X, http 타겟 O, http로 연결 -> http 타겟으로 연결
+            proxyTarget = httpTargets.random()
+        } else if (httpsTarget.length > 0 && !req.socket.server.key) {
+            // http 타겟 X, https 타겟 O, http로 연결 -> https 타겟으로 연결
+            proxyTarget = httpsTargets.random()
+        } else if (httpTarget.length > 0 && req.socket.server.key) {
+            // http 타겟 O, https 타겟 X, https로 연결 -> http 타겟으로 연결
+            proxyTarget = httpTargets.random()
+        } else {
+            res.sendStatus(404)
+            return
+        }
+
+        proxy({
+            target: proxyTarget,
+            ws: targetRoute.websocket || true,
+            changeOrigin: targetRoute.sendHost || true
+        })(req, res, next)
+    }
+
+
+});
+var httpServer = http.createServer(app).listen(80);
+var httpsServer = https.createServer(defaultCertificate, app).listen(443);
+
+
+// API Server
+// Require express and create an instance of it
 var apiapp = express();
 var uuid = require('uuid');
 
@@ -103,21 +133,30 @@ apiapp.use((req, res, next) => {
 // on the request to root (localhost:3000/)
 apiapp.get('/certificate/letsencrypt/:domain', function (req, res) {
     letsEncrypt(req.params.domain.split(','), (err, certs) => {
-        if(err){
+        if (err) {
             res.status(500)
-            res.json({'result': 'failed', 'error': err});
+            res.json({
+                'result': 'failed',
+                'error': err
+            });
         } else {
-            res.json({'result': 'success', 'certs': certs});
+            res.json({
+                'result': 'success',
+                'certs': certs
+            });
         }
     })
 });
 
 apiapp.get('/routes', (req, res) => {
-    res.json({'result': 'success', 'data': config.routing})
+    res.json({
+        'result': 'success',
+        'data': config.routing
+    })
 })
 
 apiapp.get('/routes/:routeId', (req, res) => {
-    if(config.routing[req.params.routeId]){
+    if (config.routing[req.params.routeId]) {
         res.json(config.routing[req.params.routeId])
     } else {
         res.sendStatus(404)
@@ -131,47 +170,51 @@ apiapp.post('/routes', (req, res) => {
     // Check host conflict
     let isConflict = false
     newData.host.forEach((host) => {
-        if(Object.values(config.routing).find(o => o.host.includes(host))){
-            isConflict=true;
+        if (Object.values(config.routing).find(o => o.host.includes(host))) {
+            isConflict = true;
         }
     })
-    if(isConflict){
+    if (isConflict) {
         res.sendStatus(409)
-            return
+        return
     }
     config.routing[dataUUID] = newData
     saveConfig()
-    res.setHeader('Location','/routes/' + dataUUID)
+    res.setHeader('Location', '/routes/' + dataUUID)
     res.sendStatus(201)
 })
 
 apiapp.patch('/routes/:routeId', (req, res) => {
     let newData = req.body
     let conflict = false
-    if(config.routing[req.params.routeId]){
-        if(newData.host){
+    if (config.routing[req.params.routeId]) {
+        if (newData.host) {
             // Host 중복 체크
             newData.host.forEach((host) => {
-                if(Object.keys(config.routing).find(o => config.routing[o].host.includes(host) && o != req.params.routeId)){
+                if (Object.keys(config.routing).find(o => config.routing[o].host.includes(host) && o != req.params.routeId)) {
                     conflict = true
                 }
             })
         }
-        if(conflict){
+        if (conflict) {
             res.sendStatus(409)
-                    return
+            return
         }
         let currentData = config.routing[req.params.routeId]
-        config.routing[req.params.routeId] = {...currentData, ...newData}
+        config.routing[req.params.routeId] = { ...currentData,
+            ...newData
+        }
         saveConfig()
-        res.json({'status': 'ok'})
+        res.json({
+            'status': 'ok'
+        })
     } else {
         res.sendStatus(404)
     }
 })
 
 apiapp.delete('/routes/:routeId', (req, res) => {
-    if(config.routing[req.params.routeId]){
+    if (config.routing[req.params.routeId]) {
         delete config.routing[req.params.routeId]
         saveConfig()
         res.sendStatus(200)
@@ -181,8 +224,8 @@ apiapp.delete('/routes/:routeId', (req, res) => {
 })
 
 apiapp.use(express.static('web/'))
-apiapp.use((req, res, next) =>{
-fs.createReadStream('web/index.html').pipe(res)
+apiapp.use((req, res, next) => {
+    fs.createReadStream('web/index.html').pipe(res)
 })
 
 apiapp.listen(8909, function () {
@@ -190,7 +233,7 @@ apiapp.listen(8909, function () {
     console.log('Access to it directly or set up reverse proxy with this ip')
 });
 
-function saveConfig(cb){
-if(!cb) cb = ()=>{}
-fs.writeFile('config.json', JSON.stringify(config), cb)
+function saveConfig(cb) {
+    if (!cb) cb = () => {}
+    fs.writeFile('config.json', JSON.stringify(config), cb)
 }
